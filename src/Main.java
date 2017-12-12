16f2d6a440309e5742d1cccaa0599f5489f56d82
posusaevkostay@mail.ru <posusaevkostay@mail.ru>
2017-12-12 21:34:42 +0300

    Task1

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ import static java.lang.Math.pow;
 
 public class Main {
     public static void main(String[] args) {
-        float fraction = (float) 1 / 3;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число");
         float x = scanner.nextFloat();
-        float result = (float) pow(20, (float) pow(x, fraction)) + (float) pow(158, 2);
+        float result = (float) pow(20, (float) pow(x, (float) 1 / 3)) + (float) pow(158, 2);
         System.out.println("Результат = " + result);
     }
 }
